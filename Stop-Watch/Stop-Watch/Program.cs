e628e42af68a5ea74a7fa7979cdfd942aa36f3dd
Tejassonar <sonartejas908@gmail.com>
2021-09-22 11:59:28 +0530

    Added Stop-Watch Simulation

---
+++ Stop-Watch/Stop-Watch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(1);
+        }
+        stopwatch.Stop();
+        Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+    }
+}
