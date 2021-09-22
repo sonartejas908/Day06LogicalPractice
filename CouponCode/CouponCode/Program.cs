d972d3f16716e884767acf9d2559921bd0ec4d6b
Tejassonar <sonartejas908@gmail.com>
2021-09-22 12:22:08 +0530

    Added CouponCode problem Solution

---
+++ CouponCode/CouponCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CouponCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Number of Coupon Codes");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 0; j < Num; j++)
+            {
+                string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                var stringChars = new char[6];
+                var random = new Random();
+
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+                }
+
+                var finalString = new String(stringChars);
+                Console.WriteLine("Code "+j+ " is : "+finalString);
+            }
+        }
+    }
+}
